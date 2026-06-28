cef3eceeb7fd92db700ad9755c40ffd2e202c03c
MUDASIRJR <mudasirclraxmaan6@gmail.com>
2026-06-28 20:19:36 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function add(value) {
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function calculate() {
+    let display = document.getElementById("display");
+    display.value = eval(display.value);
+}
